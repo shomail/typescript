04cace6d03ffeb5ad08cbc43d97018b8ebc6fad2
Shomail <shomail1988@gmail.com>
2019-01-04 13:07:17 +0100

    Basic type excercise

---
+++ typesExcercise.ts
@@ -0,0 +1,28 @@
+type BankAccount = {
+  money: number,
+  deposit: (val: number) => void
+}
+
+type Person = {
+  name: string,
+  bankAccount: BankAccount,
+  hobbies: string[]
+}
+
+let bankAccount: BankAccount = {
+  money: 2000,
+  deposit(value: number): void {
+      this.money += value;
+  }
+};
+
+let myself: Person = {
+  name: "Shomail",
+  bankAccount: bankAccount,
+  hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
+
