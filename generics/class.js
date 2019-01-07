bae9563c12f570b00ca2136ab0abf28b117f9dd0
Shomail <shomail1988@gmail.com>
2019-01-07 12:17:42 +0100

    generic map class example

--- generics/class.js
+++ generics/class.js
@@ -14,3 +14,34 @@ var simpleMath = new SimpleMath();
 simpleMath.baseValue = "10";
 simpleMath.multiplyValue = 4;
 console.log(simpleMath.calculate());
+//Generic Map class
+var GenMap = /** @class */ (function () {
+    function GenMap() {
+        this.map = {};
+    }
+    GenMap.prototype.setItem = function (key, item) {
+        this.map[key] = item;
+    };
+    GenMap.prototype.getItem = function (key) {
+        return this.map[key];
+    };
+    GenMap.prototype.clear = function () {
+        this.map = {};
+    };
+    GenMap.prototype.printMap = function () {
+        for (var key in this.map) {
+            console.log(key + " => " + this.map[key]);
+        }
+    };
+    return GenMap;
+}());
+var numMap = new GenMap();
+numMap.setItem('cars', 11);
+numMap.setItem('bikes', 15);
+numMap.setItem('cycles', 26);
+numMap.printMap();
+var personMap = new GenMap();
+personMap.setItem('name', 'sbt');
+personMap.setItem('game', 'call of duty');
+personMap.setItem('score', '250pts');
+personMap.printMap();
--- generics/class.ts
+++ generics/class.ts
@@ -13,3 +13,39 @@ const simpleMath = new SimpleMath<string, number>();
 simpleMath.baseValue = "10";
 simpleMath.multiplyValue = 4;
 console.log(simpleMath.calculate());
+
+//Generic Map class
+
+class GenMap<T> {
+  private map: {[key: string]: T} = {};
+
+  setItem(key: string, item: T) {
+    this.map[key] = item;
+  }
+
+  getItem(key: string) {
+    return this.map[key];
+  }
+
+  clear() {
+    this.map = {}
+  }
+
+  printMap() {
+    for(let key in this.map) {
+      console.log(`${key} => ${this.map[key]}`)
+    }
+  }
+}
+
+const numMap = new GenMap<number>();
+numMap.setItem('cars', 11);
+numMap.setItem('bikes', 15);
+numMap.setItem('cycles', 26);
+numMap.printMap();
+
+const personMap = new GenMap<string>();
+personMap.setItem('name', 'sbt');
+personMap.setItem('game', 'call of duty'); 
+personMap.setItem('score', '250pts'); 
+personMap.printMap();
