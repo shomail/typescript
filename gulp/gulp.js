d214279a650c709820014630abebe122ce897008
Shomail <shomail1988@gmail.com>
2019-01-09 20:53:58 +0100

    Project re-structure and React app scaffolding with ts

---
+++ gulp/gulp.js
@@ -0,0 +1,2 @@
+"use strict";
+var filePath = 'path/to/file.js';
--- tscode/es6.ts
+++ tscode/es6.ts
@@ -12,26 +12,26 @@ function uselessFunc() {
 }
 
 //arrow function
-const addNumbers = function (num1:number, num2:number): number {
-  return num1 + num2
-}
+const addNumbers = function(num1: number, num2: number): number {
+  return num1 + num2;
+};
 
-const subNumbers = (num1:number, num2:number): number => num1 - num2;
+const subNumbers = (num1: number, num2: number): number => num1 - num2;
 
 //default func params
 const countdown = (start: number = 10): void => {
-  console.log("COUNTDOWN START")
-  while(start > 0){
-    start --;
-    console.log(start)
+  console.log('COUNTDOWN START');
+  while (start > 0) {
+    start--;
+    console.log(start);
   }
-  console.log("COUNTDOWN END")
-}
+  console.log('COUNTDOWN END');
+};
 
 // countdown();
 
 //rest & spread ...
-const listOfNumbers = [100,99,81,11,909];
+const listOfNumbers = [100, 99, 81, 11, 909];
 //spread array in func args
 // console.log(Math.max(...listOfNumbers));
 
@@ -44,32 +44,31 @@ function makeAnArray(...args: number[]) {
 // console.log(makeAnArray(1,2,3,4,5,6))
 
 //tuples as type for rest params ts v3 specific
-function printInfo(...info: [string, number]) {
+function printInfoToConsole(...info: [string, number]) {
   console.log('My name is ' + info[0] + ' and I am ' + info[1] + ' years old!');
 }
 
 //Destructuring Array
-const favMovies = ["Avengers","Lord of the rings", "Harry potter"]
+const favMovies = ['Avengers', 'Lord of the rings', 'Harry potter'];
 const [avengers, lotr, hp] = favMovies;
-console.log(avengers, lotr, hp)
+console.log(avengers, lotr, hp);
 
 //Destructuring Objects
 
 const movieData = {
-  name: "Avengers",
+  name: 'Avengers',
   year: 2018,
   rating: 9.9
-}
+};
 
-const { name:movieName, year:movieYear } = movieData
+const { name: movieName, year: movieYear } = movieData;
 
 console.log(movieName, movieYear);
 
 //template literals
-const teamName = "Lahore Qalandars";
+const teamName = 'Lahore Qalandars';
 const teamRanking = 6;
 console.log(`Team ${teamName} got ${teamRanking}th Position in the tournament!`);
-
 
 //ES6 Exercises
 
@@ -87,22 +86,22 @@ console.log(double(10));
 //   if (name === undefined) { name = "Max"; }
 //   console.log("Hello, " + name);
 // };
-const greet = (name: string = "World"): void => {
-  console.log(`Hello, ${name}!`)
-}
+const greet = (name: string = 'World'): void => {
+  console.log(`Hello, ${name}!`);
+};
 greet();
-greet("Anna");
+greet('Anna');
 
 // Exercise 3 - Isn't there a shorter way to get all these Values?
 // var numbers = [-3, 33, 38, 5];
 // console.log(Math.min.apply(Math, numbers));
-const  uselessNumbers = [-3, 33, 38, 5];
+const uselessNumbers = [-3, 33, 38, 5];
 console.log(Math.min(...uselessNumbers));
 
 // Exercise 4 - I have to think about Exercise 3 ...
 // var newArray = [55, 20];
 // Array.prototype.push.apply(newArray, numbers);
-const newArray = [55, 20, ...uselessNumbers]
+const newArray = [55, 20, ...uselessNumbers];
 console.log(newArray);
 
 // Exercise 5 - That's a well-constructed array.
@@ -118,6 +117,6 @@ console.log(result1, result2, result3);
 // var scientist = {firstName: "Will", experience: 12};
 // var firstName = scientist.firstName;
 // var experience = scientist.experience;
-const scientist = {firstName: "Will", experience: 12};
-const {firstName: scientistFirstName, experience: scientistExperience} = scientist
+const scientist = { firstName: 'Will', experience: 12 };
+const { firstName: scientistFirstName, experience: scientistExperience } = scientist;
 console.log(scientistFirstName, scientistExperience);
